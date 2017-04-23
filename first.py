178361f10531b30d9c0a868b56deb1a771cc6e91
Anirban Ain <anirban.ain@ligo.org>
2017-04-23 14:21:33 +0530

    Pycharm edit

--- first.py
+++ first.py
@@ -24,6 +24,7 @@ H1 = pycbc.detector.Detector('H1')
 
 H1_plus_hres = np.array(H1_plus_hres)
 
+
 #fig = plt.figure()
 hp.mollview(np.absolute(H1_plus_hres), title = "H1 Plus High Res")
 plt.savefig('./H1_Plus_HR.png')
@@ -32,6 +33,7 @@ H1_cross_hres = np.array(H1_cross_hres)
 
 hp.mollview(np.absolute(H1_cross_hres), title = "H1 Cross High Res")
 plt.savefig('./H1_Cross_HR.png')
+
 
 #nside = 4
 #npix = hp.nside2npix(nside)
