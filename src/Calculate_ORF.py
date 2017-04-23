bcb8f6b820194848799d18149de8347bfe1a4171
Anirban Ain <anirban.ain@ligo.org>
2017-04-23 17:07:00 +0530

    writing orf

--- src/Calculate_ORF.py
+++ src/Calculate_ORF.py
@@ -22,9 +22,16 @@ ra_hres = phi_hres
 H1 = pycbc.detector.Detector('H1')
 L1 = pycbc.detector.Detector('L1')
 
+t_gps = 1126626073
+f = 1726
+
 # Plus and Cross Antenna Pattern for the detectors
-(H1_plus, H1_cross) = np.vectorize(H1.antenna_pattern)(ra, dec, 0, 1126626073);
-(L1_plus, L1_cross) = np.vectorize(L1.antenna_pattern)(ra, dec, 0, 1126626073);
+(H1_plus, H1_cross) = np.vectorize(H1.antenna_pattern)(ra, dec, 0, t_gps)
+(L1_plus, L1_cross) = np.vectorize(L1.antenna_pattern)(ra, dec, 0, t_gps)
 
 plus = H1_plus * L1_plus
 cross = H1_cross * L1_cross
+
+t_delay = np.vectorize(H1.time_delay_from_detector)(L1,ra_hres,dec_hres,t_gps)
+phase = 2.0 * np.pi * complex(0,1) * 1726 * t_delay
+exp_factor = np.vectorize(cmath.exp)(phase)
