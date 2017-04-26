7a4e42783d6b943f1210dd5a976f7761a67d4602
Anirban Ain <anirban.ain@ligo.org>
2017-04-26 14:53:42 +0530

    to sync

--- src/ovr_loop.py
+++ src/ovr_loop.py
@@ -11,8 +11,25 @@ import matplotlib.pyplot as plt
 
 start = time.time()
 
+param_input = open("../input/param_all.txt","r")
+
+ifo1 = "H1"
+ifo2 = "L1"
+
+nside = 1
+
+GPStime_start = 1126626073
+#GPStime_end = 1126626173
+GPStime_end = 1126712237
+segDuration = 26
+
+fLow = 20
+#fHigh = 21
+fHigh = 1726
+deltaF = 0.25
+
 # Parameters for the High Resolution Map
-nside = 2
+
 npix = hp.nside2npix(nside)
 pix = np.arange(npix)
 
@@ -23,19 +40,11 @@ dec = (np.pi/2) - theta
 ra = phi
 
 # The two LIGO detectors
-H1 = pycbc.detector.Detector('H1')
-L1 = pycbc.detector.Detector('L1')
+ifo1 = pycbc.detector.Detector(ifo1)
+ifo2 = pycbc.detector.Detector(ifo2)
 
-GPStime_start = 1126626073
-#GPStime_end = 1126626173
-GPStime_end = 1126712237
-segDuration = 26
+
 nSegment = np.int(math.floor((GPStime_end-GPStime_start)/segDuration)+1)
-
-fLow = 20
-#fHigh = 21
-fHigh = 1726
-deltaF = 0.25
 nFreqBin = np.int(math.floor((fHigh-fLow)/deltaF) + 1)
 
 combined_antenna_response = []
@@ -43,13 +52,13 @@ t_delay = []
 
 for t_gps in np.arange(GPStime_start, GPStime_end, segDuration):
 
-    (H1_plus, H1_cross) = np.vectorize(H1.antenna_pattern)(ra, dec, 0, t_gps)
-    (L1_plus, L1_cross) = np.vectorize(L1.antenna_pattern)(ra, dec, 0, t_gps)
+    (ifo1_plus, ifo1_cross) = np.vectorize(ifo1.antenna_pattern)(ra, dec, 0, t_gps)
+    (ifo2_plus, ifo2_cross) = np.vectorize(ifo2.antenna_pattern)(ra, dec, 0, t_gps)
 
-    combined_antenna_t = (H1_plus * L1_plus) + (H1_cross * L1_cross)
+    combined_antenna_t = (ifo1_plus * ifo2_plus) + (ifo1_cross * ifo2_cross)
     combined_antenna_response.append(combined_antenna_t)
 
-    t_delay_t = np.vectorize(H1.time_delay_from_detector)(L1,ra,dec,t_gps)
+    t_delay_t = np.vectorize(ifo1.time_delay_from_detector)(ifo2,ra,dec,t_gps)
     t_delay.append(t_delay_t)
 
 end = time.time()
@@ -69,11 +78,20 @@ combined_antenna_response = np.array(combined_antenna_response)
 map_final_mat = []
 
 ii = 0
-f = np.arange(fLow, fHigh+deltaF, deltaF)
+f = 2*np.pi*(np.arange(fLow, fHigh+deltaF, deltaF))
 for t in np.arange(GPStime_start, GPStime_end, segDuration):
 
-    exp_term = np.vectorize(cmath.exp)\
-    (2*np.pi*complex(0,1)*(t_delay[ii,:][:,None] * f[None,:]))
+    #exp_term = np.vectorize(cmath.exp)\
+    #(2*np.pi*complex(0,1)*(t_delay[ii,:][:,None] * f[None,:]))
+
+    #thisphase = 2*np.pi*(t_delay[ii,:][:,None] * f[None,:])
+    np.shape(t_delay)
+    np.shape(f)
+    thisphase = np.outer(t_delay, f)
+    sine_mat = np.vectorize(math.sin)(thisphase)
+    cos_mat = np.vectorize(math.cos)(thisphase)
+    exp_term =  np.vectorize(complex)(cos_mat,sine_mat)
+    #(2*np.pi*complex(0,1)*(t_delay[ii,:][:,None] * f[None,:]))
 
     f_mat = exp_term * (numpy.matlib.repmat(csd[ii,:],npix,1))
     map_t =  np.sum(f_mat, axis=1)
@@ -81,7 +99,7 @@ for t in np.arange(GPStime_start, GPStime_end, segDuration):
     ii =ii+1
     if (ii%100) == 0:
         #print (100*(ii/nSegment)),'% Done'
-        print ii, 'segments done'
+        print ii, 'segments done in', time.time()
 
     map_final_mat.append(map_t)
 
